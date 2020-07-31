b556d3acb3ee86ea1317b444360a8c1606bc5d49
Vergil Choi <vergil.choi.zyc@gmail.com>
2020-07-31 20:07:39 +0100

    add mutation operators

--- operators/mutation.py
+++ operators/mutation.py
@@ -38,7 +38,7 @@ def random_mutate(genes: List[float], rate: float, **kwargs) -> List[float]:
     return offspring
 
 
-def norm_dist(genes: List[float], rate: float, sigma: float = 0.2, **kwargs) -> List[float]:
+def partial_abs(genes: List[float], rate: float, sigma: float = 0.2, **kwargs) -> List[float]:
     """
     Randomly change a gene value by a normal distributed probability,
     center is the gene itself
@@ -55,3 +55,54 @@ def norm_dist(genes: List[float], rate: float, sigma: float = 0.2, **kwargs) -> 
             offspring[i] += random.gauss(0, sigma)
 
     return offspring
+
+
+def partial_relative(genes: List[float], rate: float, sigma: float = 0.2, **kwargs) -> List[float]:
+    """
+    Randomly change a gene value relatively
+    by a normal distributed random number.
+
+    :param genes: list of gene
+    :param rate: probablity of mutate for every gene value
+    :param sigma: sigma for normal distribution
+    :return: mutated genes
+    """
+    offspring = genes[:]
+
+    for i in range(len(offspring)):
+        if rate > random.random():
+            offspring[i] *= 1 + random.gauss(0, sigma)
+
+    return offspring
+
+
+def vector_abs(genes: List[float], rate: float, sigma: float = 0.2, **kwargs) -> List[float]:
+    """
+    Randomly change every gene value
+    by a normal distributed random number.
+
+    :param genes: list of gene
+    :param rate: probablity of mutate for every gene value
+    :param sigma: sigma for normal distribution
+    :return: mutated genes
+    """
+    if rate <= random.random():
+        return genes
+
+    return [genes[i] + random.gauss(0, sigma) for i in range(len(genes))]
+
+
+def vector_relative(genes: List[float], rate: float, sigma: float = 0.2, **kwargs) -> List[float]:
+    """
+    Randomly change every gene value relatively
+    by a normal distributed random number.
+
+    :param genes: list of gene
+    :param rate: probablity of mutate for every gene value
+    :param sigma: sigma for normal distribution
+    :return: mutated genes
+    """
+    if rate <= random.random():
+        return genes
+
+    return [genes[i] * (1 + random.gauss(0, sigma)) for i in range(len(genes))]
--- tests/test_mutation.py
+++ tests/test_mutation.py
@@ -13,11 +13,34 @@ class TestMutation(unittest.TestCase):
         self.assertLessEqual(new_genes, [1, 1, 1, 1])
         # print(new_genes)
 
-    def test_norm_dist(self):
+    def test_partial_abs(self):
         genes = [0, 1, 0.5, 0.25]
         for _i in range(100):
-            new_genes = m.norm_dist(genes, 1)
+            new_genes = m.partial_abs(genes, 1)
             self.assertEqual(len(genes), len(new_genes))
             self.assertGreaterEqual(new_genes, [-1, 0, -0.5, -0.75])
             self.assertLessEqual(new_genes, [1, 2, 1.5, 1.25])
-            # print(new_genes)
+
+    def test_partial_relative(self):
+        genes = [0, 1, 0.5, 0.25]
+        for _i in range(100):
+            new_genes = m.partial_relative(genes, 1, 0.02)
+            self.assertEqual(len(genes), len(new_genes))
+            self.assertGreaterEqual(new_genes, [0.0, 0.8, 0.4, 0.20])
+            self.assertLessEqual(new_genes, [0.0, 1.2, 0.6, 0.30])
+
+    def test_vector_abs(self):
+        genes = [0, 1, 0.5, 0.25]
+        for _i in range(100):
+            new_genes = m.vector_abs(genes, 1)
+            self.assertEqual(len(genes), len(new_genes))
+            self.assertGreaterEqual(new_genes, [-1, 0, -0.5, -0.75])
+            self.assertLessEqual(new_genes, [1, 2, 1.5, 1.25])
+
+    def test_vector_relative(self):
+        genes = [0, 1, 0.5, 0.25]
+        for _i in range(100):
+            new_genes = m.vector_relative(genes, 1, 0.02)
+            self.assertEqual(len(genes), len(new_genes))
+            self.assertGreaterEqual(new_genes, [0.0, 0.8, 0.4, 0.20])
+            self.assertLessEqual(new_genes, [0.0, 1.2, 0.6, 0.30])
